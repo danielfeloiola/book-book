9b7b2acea7517d00622b752e8da2cae3fe47e23f
Daniel Loiola <danf10@me.com>
2022-10-09 18:13:46 -0300

    moved to railway

--- application.py
+++ application.py
@@ -25,9 +25,6 @@ uri = os.getenv("DATABASE_URL")
 if not uri:
     raise RuntimeError("DATABASE_URL is not set")
 
-# adding this as heroku provides the wrong URL
-if uri and uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(uri)
 db = scoped_session(sessionmaker(bind=engine))
